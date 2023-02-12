79c34f716a3870dc3d142336a2181a64f19f96ed
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2023-02-12 08:04:47 +0100

    fix: add more headers for cors config

--- netlify/functions/reverse-geo.js
+++ netlify/functions/reverse-geo.js
@@ -1,20 +1,22 @@
 exports.handler = async function (event, context) {
     const CORS_HEADERS = {
         'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
     }
 
-    const lookup = require('coordinate_to_country');
-    const body = JSON.parse(event.body);
+    const lookup = require('coordinate_to_country')
+    const body = JSON.parse(event.body)
 
-    let countries = [];
+    let countries = []
 
     body.forEach(item => {
-        countries.push(lookup(item.lat, item.lon, true)[0]);
+        countries.push(lookup(item.lat, item.lon, true)[0])
     })
 
     return {
         headers: CORS_HEADERS,
         statusCode: 200,
         body: JSON.stringify(countries),
-    };
+    }
 }
