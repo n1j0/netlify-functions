a83be590e5b103344854ba28296280ff4f94041a
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2023-02-12 05:32:27 +0100

    fix(geo): use older es style

--- netlify/functions/reverse-geo.js
+++ netlify/functions/reverse-geo.js
@@ -1,17 +1,18 @@
 exports.handler = async function (event, context) {
-    const lookup = require('coordinate_to_country')
-    const { lat, lon } = event.queryStringParameters
+    const lookup = require('coordinate_to_country');
+    const lat = event.queryStringParameters.lat;
+    const lon = event.queryStringParameters.lon;
 
     if (!lat || !lon) {
         return {
             statusCode: 400,
             body: JSON.stringify({ error: 'lat and lon are required' }),
-        }
+        };
     }
 
-    const country = lookup(fp.latitude, fp.longitude, true)[0]
+    const country = lookup(lat, lon, true)[0];
     return {
         statusCode: 200,
         body: JSON.stringify(country),
-    }
+    };
 }
