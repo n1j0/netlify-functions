5ff3ea315a1593f6932f34fa152c1355e9cc216b
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2023-02-12 07:45:18 +0100

    fix: set cors headers

--- netlify/functions/reverse-geo.js
+++ netlify/functions/reverse-geo.js
@@ -1,10 +1,15 @@
 exports.handler = async function (event, context) {
+    const CORS_HEADERS = {
+        'Access-Control-Allow-Origin': '*',
+    }
+
     const lookup = require('coordinate_to_country');
     const lat = event.queryStringParameters.lat;
     const lon = event.queryStringParameters.lon;
 
     if (!lat || !lon) {
         return {
+            headers: CORS_HEADERS,
             statusCode: 400,
             body: JSON.stringify({ error: 'lat and lon are required' }),
         };
@@ -12,6 +17,7 @@ exports.handler = async function (event, context) {
 
     const country = lookup(lat, lon, true)[0];
     return {
+        headers: CORS_HEADERS,
         statusCode: 200,
         body: JSON.stringify(country),
     };
